8bbb6f9a27d289f1ffc44f28b1bc9af93194f598
ulsemsee2 <mrodney@kabarak.ac.ke>
2020-03-21 17:56:18 +0300

    task1

---
+++ courses.js
@@ -0,0 +1,10 @@
+let name = "Rodney Makori";
+let courses = ["html", "css", "js", "design", "node js"];
+
+console.log(name, courses);
+
+for (let a = 0; a <= 200; a++) {
+  if (a % 3 == 0) {
+    console.log(a);
+  }
+}
